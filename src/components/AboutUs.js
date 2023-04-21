babe22c6b90076e447e6dfe62b34c791a253b741
E Luckie <eluckie.d@gmail.com>
2023-04-21 15:38:17 -0600

    add styling to about us & spotlight pages

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,21 +1,31 @@
 import React from "react";
+import Icon from "./../img/flower-icon.png";
 
 function AboutUs() {
   return (
     <React.Fragment>
-      <h1>flower to the people</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <br/>
-      <h2>hours</h2>
-      <h3>
-        mon-sun<br/>
-        8a-8p
-      </h3>
-      <h4>
-        888 flower ln<br/>
-        flower city, flower state<br/>
-        888-888-8888
-      </h4>
+      <div className="center">
+        <h1 id="yellow-accent">flower to the people</h1>
+        <p id="blurb">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <br/><br/>
+        <div id="details">
+          <h2 id="purple-accent">hours</h2>
+          <h3>
+            mon-sun<br/>
+            8a-8p
+          </h3>
+          <h4>
+            888 flower ln<br/>
+            flower city, flower state<br/>
+            888-888-8888
+          </h4>
+        </div>
+        <br/><br/>
+        <img id="icon-border" src={Icon} alt="site icon"/>
+        <img id="icon-border" src={Icon} alt="site icon"/>
+        <img id="icon-border" src={Icon} alt="site icon"/>
+        <img id="icon-border" src={Icon} alt="site icon"/>
+      </div>
     </React.Fragment>
   );
 }
--- src/components/Spotlight.js
+++ src/components/Spotlight.js
@@ -2,6 +2,7 @@ import React from "react";
 import Spot1Img from "./../img/spotlight1.png";
 import Spot2Img from "./../img/spotlight2.png";
 import Spot3Img from "./../img/spotlight3.png";
+import Icon from "./../img/flower-icon.png";
 
 function Spotlight() {
   return (
@@ -9,21 +10,24 @@ function Spotlight() {
       <div id="spotlight-wrapper">
         <div id="spotlight">
           <img src={Spot1Img} alt="bud icon smiling showing peace signs"/>
-          <h2>harry hybrid</h2>
+          <h2 id="purple-accent">harry hybrid</h2>
           <p><em>from</em> happy valley</p>
-          <p><strong>$4 per flower</strong></p>
+          <p><strong>$4</strong>  per flower</p>
+          <img id="small-icon" src={Icon} alt="site icon"/>
         </div>
         <div id="spotlight">
           <img src={Spot2Img} alt="bud icon in om pose"/>
-          <h2>isaiah indi</h2>
+          <h2 id="purple-accent">isaiah indi</h2>
           <p><em>from</em> in your dreams</p>
           <p><strong>$2 per flower</strong></p>
+          <img id="small-icon" src={Icon} alt="site icon"/>
         </div>
         <div id="spotlight">
           <img src={Spot3Img} alt="bud icon flexing arm muscles"/>
-          <h2>sally sativa</h2>
+          <h2 id="purple-accent">sally sativa</h2>
           <p><em>from</em> strong-arm central</p>
           <p><strong>$0 per flower</strong></p>
+          <img id="small-icon" src={Icon} alt="site icon"/>
         </div>
       </div>
     </React.Fragment>
